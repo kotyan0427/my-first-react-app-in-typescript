95cae8a4e3e0723993b0b64effb467053e519b76
kotyan0427 <koulotte0427@outlook.jp>
2021-04-30 18:20:56 +0900

    useRefとuseEffect

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const Counter: React.VFC<{}> = () => {
-  const initialValue: any = 0;
-  const [value, setValue] = useState<number>(initialValue);
+  const [value, setValue] = useState<number>(0);
+  const refTimes = useRef<number>(0);
+  useEffect(() => {
+    refTimes.current = refTimes.current + 1;
+  });
 
   const increment = () => {
     setValue((prev) => prev + 1);
@@ -17,6 +20,7 @@ export const Counter: React.VFC<{}> = () => {
       <div>value:{value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {refTimes.current} times!</div>
     </div>
   );
 };
